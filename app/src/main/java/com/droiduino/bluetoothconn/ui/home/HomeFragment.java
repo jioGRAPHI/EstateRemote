7e59cca207d953816b1f3a8bd27a87d2a74bc0ea
jioGRAPHI <jvcaraan@up.edu.ph>
2023-03-30 18:13:55 +0800

    Initial Commit

---
+++ app/src/main/java/com/droiduino/bluetoothconn/ui/home/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.droiduino.bluetoothconn.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.droiduino.bluetoothconn.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
